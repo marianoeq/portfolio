a7c4e9fea0b34befe913a71231d2a74609408828
marian.eq <mariano.eq@gmail.com>
2022-04-26 21:44:37 +0200

    Target blank added

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -3,17 +3,38 @@ import "./style.css";
 
 function ProjectCard(props) {
   return (
-    <div className="card border-dark mb-3 grid-item"> 
-      <img className="card-img-top" src={props.screenshot} alt={props.name}></img>
+    <div className="card border-dark mb-3 grid-item">
+      <img
+        className="card-img-top"
+        src={props.screenshot}
+        alt={props.name}
+      ></img>
       <div className="card-body">
-        <h5 className="card-title">&#123; <span className='objKey'>{props.name}</span>:</h5>
-        <p className="card-text"><span className="objString">"{props.description}"</span> &#125;</p>
-        <a href={props.repo} target="_blank" rel="noreferrer noopener"className="btn btn-dark btn-rounded"><strong>Repo</strong></a>
-        <a href={props.deployedURL} target="_blank" rel="noreferrer noopener" className="btn btn-dark btn-rounded"><strong>Live URL</strong></a>
+        <h5 className="card-title">
+          &#123; <span className="objKey">{props.name}</span>:
+        </h5>
+        <p className="card-text">
+          <span className="objString">"{props.description}"</span> &#125;
+        </p>
+        <a
+          href={props.repo}
+          target="_blank"
+          rel="noreferrer noopener"
+          className="btn btn-dark btn-rounded"
+        >
+          <strong>Repo</strong>
+        </a>
+        <a
+          href={props.deployedURL}
+          target="_blank"
+          rel="noreferrer noopener"
+          className="btn btn-dark btn-rounded"
+        >
+          <strong>Live URL</strong>
+        </a>
       </div>
     </div>
   );
 }
-
 
 export default ProjectCard;
